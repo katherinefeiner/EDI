0ea8c84f3e734c7c2580b2cc6b76705a2eedd5a2
katherinefeiner <kfeiner212@gmail.com>
2024-01-08 08:58:50 -0500

    2x2

--- Creative Coding/2x2/sketch.js
+++ Creative Coding/2x2/sketch.js
@@ -1,7 +1,41 @@
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(720,720);
+  background('lightblue');
+  fill('lightpink');
+  strokeWeight (9);
+
 }
 
 function draw() {
-  background(220);
+  
+  translate(60,60); //resets origin point
+  
+  quad (
+  0,0,
+    300,0,
+    300,300,
+    0,300
+  );
+  
+  quad (
+  300,0,
+    600,0,
+    600,300,
+    300,300
+  ); //move 300 pixels in the x
+  
+   quad (
+  0,300,
+    300,300,
+    300,600,
+    0,600 
+  ); //move 300 in the y
+  
+  quad (
+  300,300,
+    600,300,
+    600,600,
+    300,600
+  ); //move 300 pixels in the y
+
 }
--- Creative Coding/square/sketch.js
+++ Creative Coding/square/sketch.js
@@ -1,7 +1,20 @@
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(720,720);
+  background('lightblue');
+  fill('lightpink');
+  strokeWeight (9);
+
 }
 
 function draw() {
-  background(220);
+  
+  translate(60,60); //resets origin point
+  
+  quad (
+  0,0,
+    300,0,
+    300,300,
+    0,300
+  );
+
 }
